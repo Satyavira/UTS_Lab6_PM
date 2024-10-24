3f99b72c4d245dc2733a0b6f1fafe20fb0679ace
Satyavira <viraprathama@gmail.com>
2024-10-25 00:30:27 +0700

    Change logo to the res folder, create LoadingActivity.kt; activity_loading.xml; logo_1.jpg; gradient_background.xml, update AndroidManifest.xml and build.gradle.kts

---
+++ app/src/main/java/com/satyavira/uts_lab6_pm/LoadingActivity.kt
@@ -0,0 +1,27 @@
+package com.satyavira.uts_lab6_pm
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.satyavira.uts_lab6_pm.databinding.ActivityLoadingBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class LoadingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoadingBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityLoadingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(5000)
+            val intent = Intent(this@LoadingActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
