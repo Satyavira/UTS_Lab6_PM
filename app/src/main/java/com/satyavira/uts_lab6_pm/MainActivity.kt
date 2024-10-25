e1b50d3686f419daa578d993d9526b3cfe8e9843
Satyavira <viraprathama@gmail.com>
2024-10-25 16:40:17 +0700

    Modified MainActivity.kt (binding, button onclick listener), strings.xml, activity_main.xml, and create CreateGreetingCardActivity.kt, activity_create_greeting_card.xml.

--- app/src/main/java/com/satyavira/uts_lab6_pm/MainActivity.kt
+++ app/src/main/java/com/satyavira/uts_lab6_pm/MainActivity.kt
@@ -1,15 +1,35 @@
 package com.satyavira.uts_lab6_pm
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.satyavira.uts_lab6_pm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btCard1.setOnClickListener {
+            goToCreateGreetingCardActivity(1)
+        }
+        binding.btCard2.setOnClickListener {
+            goToCreateGreetingCardActivity(2)
+        }
+        binding.btCard3.setOnClickListener {
+            goToCreateGreetingCardActivity(3)
+        }
+    }
+    private fun goToCreateGreetingCardActivity(greetingCard: Int) {
+        val intent = Intent(this@MainActivity, CreateGreetingCardActivity::class.java)
+        intent.putExtra("Greeting Card", greetingCard)
+        startActivity(intent)
     }
 }
